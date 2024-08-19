ea369a3ae9cc5f7d073c7111ca077d1bd3cc0b1e
Gaber Sultan <g.sultan@techno-island.com>
2024-08-19 15:47:08 +0300

    add layout in project

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import {registerLicense} from "@syncfusion/ej2-base"
+registerLicense('ORg4AjUWIQA/Gnt2U1hhQlJBfVZdXGtWfFN0QXNcdV5zflRPcC0sT3RfQFljQX9adkBnW3pYdHRcRA==');
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
